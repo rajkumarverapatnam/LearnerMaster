ad4da20f7496da8e88bba82324695828a76c265f
venkyraj55 <venkyraj55@gmail.com>
2020-09-04 22:50:03 +0530

    Updates

---
+++ precache-manifest.15a41bae5fd74732d0bcb7c239ee785f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02eebb405f4c0ed3c55bbfb43d24c5b",
+    "url": "/LearnerMaster/index.html"
+  },
+  {
+    "revision": "9444de7788bceb6284d8",
+    "url": "/LearnerMaster/static/css/2.8565c925.chunk.css"
+  },
+  {
+    "revision": "ae222e3f1ca1d09160df",
+    "url": "/LearnerMaster/static/css/main.f706d390.chunk.css"
+  },
+  {
+    "revision": "9444de7788bceb6284d8",
+    "url": "/LearnerMaster/static/js/2.6c70a85d.chunk.js"
+  },
+  {
+    "revision": "1c35057aed30085c10c811a6c8dd623b",
+    "url": "/LearnerMaster/static/js/2.6c70a85d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae222e3f1ca1d09160df",
+    "url": "/LearnerMaster/static/js/main.0f193a49.chunk.js"
+  },
+  {
+    "revision": "9760cebdaf70dad41651",
+    "url": "/LearnerMaster/static/js/runtime-main.64f23b2f.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/LearnerMaster/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/LearnerMaster/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/LearnerMaster/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/LearnerMaster/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/LearnerMaster/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
