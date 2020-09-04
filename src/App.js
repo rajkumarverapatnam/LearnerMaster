d851bd1fbb3c1099e82f42210f417ee3808e8190
venkyraj55 <venkyraj55@gmail.com>
2020-09-04 23:19:12 +0530

    addign base path to support react router with github pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
-import { Route, Router, Switch } from 'react-router-dom';
+import { Route, Router, Switch } from "react-router-dom";
 
 import { PATHS } from "./routing/paths_constants";
 import "./App.css";
 import Login from "./components/login/login";
 import LearnerList from "./components/learner_list/learner_list";
 import LearnerStats from "./components/learner_stats/learner_stats";
-import { memoryRoutingHistory } from './routing/history';
+import { memoryRoutingHistory } from "./routing/history";
 
 function App() {
   return (
-    <Router history={memoryRoutingHistory}>
+    <Router history={memoryRoutingHistory} basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/" component={Login} />
-        <Route  path={PATHS.Login} component={Login} />
-        <Route  path={PATHS.Students} component={LearnerList} />
-        <Route  path={PATHS.Student + "/:id"} component={LearnerStats} />
-        </Switch>
+        <Route path={PATHS.Login} component={Login} />
+        <Route path={PATHS.Students} component={LearnerList} />
+        <Route path={PATHS.Student + "/:id"} component={LearnerStats} />
+      </Switch>
     </Router>
   );
 }
